c8f3bd8be2e4b63f3114855d0e880252f66ec438
Bu Sun Kim <busunkim@google.com>
2021-04-02 20:24:09 +0000

    chore: regenerate with create_key

--- noxfile.py
+++ noxfile.py
@@ -21,7 +21,7 @@ import nox
 BLACK_VERSION = "black==19.3b0"
 
 
-@nox.session(python="3.6")
+@nox.session(python="3.8")
 def blacken(session):
     """Run black.
     Format code to uniform standard.
@@ -60,36 +60,6 @@ def docs(session):
         os.path.join("docs", ""),
         os.path.join("docs", "_build", "html", ""),
     )
-
-
-# @nox.session(python="3.8")
-# def generate_protos(session):
-#     """Generates the protos using protoc.
-    
-#     Some notes on the `google` directory:
-#     1. The `_pb2.py` files are produced by protoc. 
-#     2. The .proto files are non-functional but are left in the repository
-#        to make it easier to understand diffs.
-#     3. The `google` directory also has `__init__.py` files to create proper modules. 
-#        If a new subdirectory is added, you will need to create more `__init__.py`
-#        files.
-
-#     NOTE: This is a hack and only runnable locally. You will need to have
-#     the api-common-protos repo cloned. This should be migrated to use
-#     bazel in the future.
-#     """
-#     session.install("grpcio-tools")
-#     protos = [str(p) for p in (Path(".").glob("google/**/*.proto"))]
-
-#     session.run(
-#         "python",
-#         "-m",
-#         "grpc_tools.protoc",
-#         "--proto_path=../api-common-protos",
-#         "--proto_path=.",
-#         "--python_out=.",
-#         *protos,
-#     )
 
 
 def default(session):
@@ -164,7 +134,7 @@ def test(session, library):
     """Run tests from a downstream libraries.
     To verify that any changes we make here will not break downstream libraries, clone
     a few and run their unit and system tests.
-    NOTE: The unit and system test functions above are copied from the templates. 
+    NOTE: The unit and system test functions above are copied from the templates.
     They will need to be updated when the templates change.
     """
     try:
@@ -183,3 +153,33 @@ def test(session, library):
     # system tests are run 3.7 only
     if session.python == "3.7":
         system(session)
+
+
+@nox.session(python="3.8")
+def generate_protos(session):
+    """Generates the protos using protoc.
+
+    Some notes on the `google` directory:
+    1. The `_pb2.py` files are produced by protoc.
+    2. The .proto files are non-functional but are left in the repository
+       to make it easier to understand diffs.
+    3. The `google` directory also has `__init__.py` files to create proper modules.
+       If a new subdirectory is added, you will need to create more `__init__.py`
+       files.
+
+    NOTE: This is a hack and only runnable locally. You will need to have
+    the api-common-protos repo cloned. This should be migrated to use
+    bazel in the future.
+    """
+    session.install("grpcio-tools")
+    protos = [str(p) for p in (Path(".").glob("google/**/*.proto"))]
+
+    session.run(
+        "python",
+        "-m",
+        "grpc_tools.protoc",
+        "--proto_path=../api-common-protos",
+        "--proto_path=.",
+        "--python_out=.",
+        *protos,
+    )
